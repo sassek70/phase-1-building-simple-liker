1a8fcf070b28487e3add3f232929df46ebfb8311
Kevin Sasse <sassek70@gmail.com>
2022-09-26 22:11:29 -0400

    finished lab

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+const modal = document.getElementById('modal')
+let heartArray = document.getElementsByClassName('like-glyph')
+let likeArray = document.getElementsByClassName('like')
+modal.classList = "hidden"
 // Your JavaScript code goes here!
 
 
+for(let heartGlyph of heartArray){
+  heartGlyph.addEventListener('click', (e) =>{
+    mimicServerCall()
+    .then(() => {
+      if(heartGlyph.textContent === '♥'){
+        heartGlyph.removeAttribute('activated-heart')
+        heartGlyph.textContent = '♡'
+        heartGlyph.style.color = "rgb(170,184,194)"
+        console.log('I\'ve been unliked')
+      } else {
+        heartGlyph.textContent = '♥'
+        heartGlyph.style.color = "red"
+        heartGlyph.classList = ('activated-heart')
+        console.log('I\'ve been liked')
+      }
+    })
+  .catch((error) => {
+    console.error(error)
+    modal.classList.remove('hidden')
+    setTimeout(() => modal.classList = "hidden",3000)}
+  )
+})
+
+}
 
 
 //------------------------------------------------------------------------------
@@ -23,3 +50,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
